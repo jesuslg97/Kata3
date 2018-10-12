8e72eee1442b347b3fbbe92ce9e110272073a669
jelop <jelop@DESKTOP-NHB31NF>
2018-10-12 12:25:46 +0100

    Kata3 version 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -12,8 +12,11 @@ import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame{
     
-    public HistogramDisplay(String title) {
+    private final Histogram <String> histogram;
+    
+    public HistogramDisplay(String title, Histogram <String> histogram) {
         super("HISTOGRAMA");
+        this.histogram = histogram;
         setContentPane(createPanel());
         pack();
     }
@@ -34,11 +37,10 @@ public class HistogramDisplay extends ApplicationFrame{
     
     private DefaultCategoryDataset createDatase() {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(5, "", "gmail.com");
-        dataSet.addValue(10, "", "ulpgc.es");
-        dataSet.addValue(7, "", "ull.es");
-        dataSet.addValue(2, "", "hotmail.com");
+        
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
         return dataSet;
     }
-    
 }
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -4,8 +4,24 @@ public class Kata3 {
     
     public static void main(String[] args) {
         
-     HistogramDisplay histo = new HistogramDisplay("JFreeChart");
-     histo.execute();
+     //HistogramDisplay histo = new HistogramDisplay("JFreeChart");
+     //histo.execute();
+        Histogram <String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+     
+        new HistogramDisplay("Hola", histogram).execute();
     }
     
 }
